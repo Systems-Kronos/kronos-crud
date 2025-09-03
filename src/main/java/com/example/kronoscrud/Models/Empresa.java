c2303eac744c5943cb0cc63425bb24f04ad74f72
sarahstxs <rebeccasarah.book2@gmail.com>
2025-09-03 18:06:24 -0300

    Commit Inicial

---
+++ src/main/java/com/example/kronoscrud/Models/Empresa.java
@@ -0,0 +1,16 @@
+package com.example.kronoscrud.Models;
+
+import java.time.LocalTime;
+
+public class Empresa {
+    private String nome;
+    private String CEP;
+    private String regraDeNegocios;
+    private String email;
+    private String CNPJ;
+    private String porte;
+    private String telefone;
+    private LocalTime horarioAbertura;
+    private LocalTime horarioFechamento;
+    private Endereco endereco;
+}
---
+++ src/main/java/com/example/kronoscrud/Models/Endereco.java
@@ -0,0 +1,9 @@
+package com.example.kronoscrud.Models;
+
+public class Endereco {
+    private String rua;
+    private String cidade;
+    private String numero;
+    private String complemento;
+    private Character estado;
+}
