68e90b06e76201f5f4b832b978181e313b708df7
matheus-orestes <matheusorestessantos@gmail.com>
2025-10-13 23:37:10 -0300

    feat/read-plano lendo todos os planos do banco

--- src/main/java/com.example/dao/PlanoDAO.java
+++ src/main/java/com.example/dao/PlanoDAO.java
@@ -1,11 +1,15 @@
 package com.example.dao;
 
 import com.example.Controller.Conexao;
+import com.example.Model.Empresa;
 import com.example.Model.Plano;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class PlanoDAO {
      //    Create
@@ -45,5 +49,43 @@ public class PlanoDAO {
 
             }
         }
+
+//        READ ALL
+        public List<Plano> read() {
+            Conexao conexao = new Conexao();
+            Connection conn = null;
+            PreparedStatement pstmt = null;
+            ResultSet rset = null;
+            String read = "SELECT * FROM planos";
+            List<Plano> listaPlano = new LinkedList<>();
+
+            try {
+                conn = conexao.conectar();
+                pstmt = conn.prepareStatement(read);
+                rset = pstmt.executeQuery();
+
+                while (rset.next()) {
+                    Plano plano = new Plano(rset.getInt("id"),
+                            rset.getString("nomeplano"),
+                            rset.getFloat("custo"),
+                            rset.getString("descricao"),
+                            rset.getInt("qnt_max_funcionario"));
+                    listaPlano.add(plano);
+                }
+            } catch (SQLException e) {
+                System.err.println("Erro ao buscar empresas: " + e.getMessage());
+                return null;
+            } finally {
+                try {
+                    if (rset != null) rset.close();
+                    if (pstmt != null) pstmt.close();
+                    if (conn != null) conn.close();
+                } catch (SQLException e) {
+                    System.err.println("Erro ao fechar recursos ao buscar empresas: " + e.getMessage());
+                }
+            }
+
+            return listaPlano;
+            }
     }
 
