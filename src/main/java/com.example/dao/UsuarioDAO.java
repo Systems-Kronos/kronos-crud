d70174e6d071130e996095c98249405855f07f34
matheus-orestes <matheusorestessantos@gmail.com>
2025-10-15 18:18:54 -0300

    feat/ metodo read-usuario de toda a tabela

--- src/main/java/com.example/dao/UsuarioDAO.java
+++ src/main/java/com.example/dao/UsuarioDAO.java
@@ -1,12 +1,16 @@
 package com.example.dao;
 
 import com.example.Controller.Conexao;
+import com.example.Model.Empresa;
 import com.example.Model.Setor;
 import com.example.Model.Usuario;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedList;
+import java.util.List;
 
 public class UsuarioDAO {
     public boolean create(Usuario usuario) {
@@ -49,4 +53,45 @@ public class UsuarioDAO {
         }
     }
 
+//    READ ALL
+    public List<Usuario> read() {
+        Conexao conexao = new Conexao();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+        String read = "SELECT * FROM usuario";
+        List<Usuario> listaUsuario = new LinkedList<>();
+
+        try {
+            conn = conexao.conectar();
+            pstmt = conn.prepareStatement(read);
+            rset = pstmt.executeQuery();
+
+            while (rset.next()) {
+                Usuario usuario = new Usuario(rset.getInt("id"),
+                        rset.getString("nome"),
+                        rset.getString("cpf"),
+                        rset.getString("genero").charAt(0),
+                        rset.getString("status"),
+                        rset.getString("senha"),
+                        rset.getInt("fk_setor_id"),
+                        rset.getInt("fk_supervisor_id"),
+                        rset.getString("cargo"));
+                listaUsuario.add(usuario);
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao buscar usuario: " + e.getMessage());
+            return null;
+        } finally {
+            try {
+                if (rset != null) rset.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos ao buscar usuario: " + e.getMessage());
+            }
+        }
+
+        return listaUsuario;
+    }
 }
