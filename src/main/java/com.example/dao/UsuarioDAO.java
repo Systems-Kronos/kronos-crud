92c4a4efdbdb6641d3a2a0b08e900208e6a74dfb
matheus-orestes <matheusorestessantos@gmail.com>
2025-10-15 18:21:42 -0300

    feat/readById-usuario

--- src/main/java/com.example/dao/UsuarioDAO.java
+++ src/main/java/com.example/dao/UsuarioDAO.java
@@ -94,4 +94,43 @@ public class UsuarioDAO {
 
         return listaUsuario;
     }
+
+//    READ by id
+    public Usuario read(int id) {
+        Conexao conexao = new Conexao();
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rset = null;
+        String readId = "SELECT * FROM usuario WHERE id = ?";
+
+        try {
+            conn = conexao.conectar();
+            pstmt = conn.prepareStatement(readId);
+            pstmt.setInt(1, id);
+            rset = pstmt.executeQuery();
+
+            if (rset.next()) {
+                return new Usuario(rset.getInt("id"),
+                        rset.getString("nome"),
+                        rset.getString("cpf"),
+                        rset.getString("genero").charAt(0),
+                        rset.getString("status"),
+                        rset.getString("senha"),
+                        rset.getInt("fk_setor_id"),
+                        rset.getInt("fk_supervisor_id"),
+                        rset.getString("cargo"));
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao buscar usuario por ID: " + e.getMessage());
+        } finally {
+            try {
+                if (rset != null) rset.close();
+                if (pstmt != null) pstmt.close();
+                if (conn != null) conn.close();
+            } catch (SQLException e) {
+                System.err.println("Erro ao fechar recursos ao buscar usuario por ID: " + e.getMessage());
+            }
+        }
+        return null;
+    }
 }
